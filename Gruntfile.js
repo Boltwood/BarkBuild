228b0ba09de98934b4f2d45febf479593784099b
bradley <bradley.b09@live.co.uk>
2021-11-17 14:43:41 +0000

    Update grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,40 @@ module.exports = function(grunt) {
                 "src": ["js/bark.js"],
                 "dest": "js/app.js"
             }
-        }
+        },
+        sass: {
+            dist: {
+              options: {
+                sourcemap: false,
+                compress: false,
+                yuicompress: false,
+                style: 'expanded',
+              },
+              files: {
+                'css/main.css' : 'main.scss',
+                'css/bark.css' : 'bark.scss',
+                'css/var.css' : 'var.scss'
+              }
+            },
+          },
+          watch: {
+            css: {
+              files: '**/*.scss',
+              tasks: ['sass']
+            }
+          }
+        
     });
 
     // Load required modules
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
 
     // Task definitions
     grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat']);
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
 };
