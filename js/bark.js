3a6ec4870e476f55aa9bc31037bfbae173b5786d
bradley <bradley.b09@live.co.uk>
2021-11-17 09:45:55 +0000

    updated overlay and js click functions

--- js/bark.js
+++ js/bark.js
@@ -6,8 +6,13 @@ window.addEventListener('load', function() {
         document.querySelector(".bark-cart.is-hidden").classList.remove("is-hidden"); 
     });
 
-    const closeCart = document.querySelector('.bark-cart__close') // Get element from DOM
+    const closeCart = document.querySelector('.bark-cart__close')// Get element from DOM
+    const overlayCart = document.querySelector('.bark-cart__overlay ')    
     closeCart.addEventListener('click', function() {
+        console.log('test');
+        document.querySelector(".bark-cart").classList.add("is-hidden"); 
+    });
+    overlayCart.addEventListener('click', function() {
         console.log('test');
         document.querySelector(".bark-cart").classList.add("is-hidden"); 
     });
