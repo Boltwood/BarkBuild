7c8aa3204500d691c9efe31350543a51143f2743
bradley <bradley.b09@live.co.uk>
2021-11-17 14:10:20 +0000

    Added cart loader and basket count

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ window.addEventListener('load', function() {
     const openCart = document.querySelector('.bark-block__btn') // Get element from DOM
     openCart.addEventListener('click', function() {
         console.log('test');
+        document.querySelector(".bark-loading").classList.remove("is-hidden"); 
+        setTimeout(function(){ 
         document.querySelector(".bark-cart.is-hidden").classList.remove("is-hidden"); 
+        document.querySelector(".bark-loading").classList.add("is-hidden"); 
+    }, 1000);
     });
 
     const closeCart = document.querySelector('.bark-cart__close')// Get element from DOM
